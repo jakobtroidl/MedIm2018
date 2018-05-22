81a7e67ffb5d84d8f05c4917528f719826397634
olliwisly <39298794+olliwisly@users.noreply.github.com>
2018-05-22 12:35:17 +0200

    Update ourCov.m

--- ourCov.m
+++ ourCov.m
@@ -1,19 +1,16 @@
-%author: oliver
 function [ ausgabe ] = ourCov( matrix, withMean )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-    med=mean(transpose(matrix));
+    med=mean(transpose(matrix)); %Mittelwerte berechnen
     rep = repmat(transpose(med),1,size(matrix,2));
-    matomed = matrix - withMean .* rep;
+    matomed=matrix-withMean.*rep; %Mittelwerte von der Matrix subtrahieren
     
+    %Kovarianzen berechnen: Cov(x(i),x(j))
     for i=1:size(matomed,1)
        for j=i:size(matomed,1)
-           ausgabe(j,i) = sum(matomed(i,:) .* (matomed(j,:))) / (size(matomed,2)-1);
+           ausgabe(j,i)=sum(matomed(i,:).*(matomed(j,:)))/(size(matomed,2)-1); %untere Dreiecksmatrix berechnen wobei in der Hauptdiagonale die Varianzen stehen (Cov(x(i),x(i))=Var(x(i)))
            if i~=j
-               ausgabe(i,j)=ausgabe(j,i);
+               ausgabe(i,j)=ausgabe(j,i); %obere Dreiecksmatrix auffüllen, da die Kovarianzmatrix symmetrisch ist
            end 
        end     
     end
     
 end
-
